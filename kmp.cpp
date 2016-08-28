2eb0d0ef8a9e74c49b38b278cbfa5af1961f569d
williamking5 <liuyongbin.ouc@gmail.com>
2016-08-28 20:43:34 +0800

    更加快捷的kmp算法实现

--- kmp.cpp
+++ kmp.cpp
@@ -2,50 +2,51 @@
 #include<string.h>
 #include<iostream>
 #define LEN 100000
-class kmpstring{
-public:
-	char s[LEN],p[LEN];
-	int next[LEN];
-	void getnext(char *p){
-		next[0]=0;
-		next[1]=0;
-		int k=0;
-		for (int q=2;q<=strlen(p+1);q++){
-			while(k>0&&p[k+1]!=p[q])
-				k=next[k];
-			if (p[k+1]==p[q]) k++;
-			next[q]=k;
-		}
+using namespace std;
+//将字符串p的next数组求出来存在ne数组中
+void getnext(char *p,int ne[]){
+	ne[0]=0;
+	ne[1]=0;
+	int k=0;
+
+	int l=strlen(p+1);
+	for (int q=2;q<=l;q++){
+		while(k>0&&p[k+1]!=p[q])
+			k=ne[k];
+		if (p[k+1]==p[q]) k++;
+		ne[q]=k;
 	}
-	int kmp(char *x,char *y,int* ans){
-		strcpy(s+1,x);
-		strcpy(p+1,y);
-		getnext(p);
-		int sum=-1,i=0,j=0,n=strlen(s+1),m=strlen(p+1);
-		while (i<=n&&j<=m){
-			if (j==m) {
-				ans[++sum]=i-j;
-				j=next[j];
-			}
-			if (p[j+1]==s[i+1]){
-				j++;i++;
-			}
+}
+//x为模式串，y为子串，ne为y的next数组,ans为匹配位置数组
+int kmp(char *x,char *y,int ne[],int ans[]){
+	int sum=-1,i=0,j=0,n=strlen(x+1),m=strlen(y+1);
+	while (i<=n&&j<=m){
+		if (j==m) {
+			ans[++sum]=i-j;
+			j=ne[j];
+		}
+		if (y[j+1]==x[i+1]){
+			j++;i++;
+		}
+		else {
+			if (!j) i++;
 			else {
-				if (!j) i++;
-				else {
-					j=next[j];
-				}
+				j=ne[j];
 			}
 		}
-		return sum;
 	}
-} test;
+	return sum;
+}
+int ne[LEN],ans[LEN];
+char s[LEN],p[LEN];
 int main(){
-	char s[100],p[100];
-	scanf("%s%s",s,p);
+	scanf("%s%s",s+1,p+1);
 	int ans[100];
-	int sum=test.kmp(s,p,ans);
+	getnext(p,ne);
+	int sum=kmp(s,p,ne,ans);
+
 	for (int i=0;i<=sum;i++)
 		cout<<ans[i]<<' ';
 	return 0;
 }
+
