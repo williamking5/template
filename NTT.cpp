bcc93f5781020c17e249fbbb00957467013b97c1
williamking5 <liuyongbin.ouc@gmail.com>
2016-11-12 03:43:40 +0800

    （最新版）快速数论变换，快速求原根

--- NTT.cpp
+++ NTT.cpp
@@ -1,20 +1,17 @@
-#include <iostream>
-#include <string.h>
-#include <stdio.h>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<string.h>
+#include<stdio.h>
 using namespace std;
 //规模N
-const long long N = (1<<18)+1;
+const long long N = (1<<14)+1;
 //采用的模数P必须为c*2^x+1的形式
-const long long P = 119*(  1<< 23) + 1;
+const long long P = 1004535809;
 //模P的最小原根G
 const long long G = 3;
-const long long NUM = 20;
+const long long NUM = 21;
 long long  wn[NUM];
-long long  a[N], b[N],c[N],f[N];
-long long A[N];
-long long inv[N],fac[N],facinv[N],POW[N],powinv[N];
-//快速幂
 long long power(long long A,long long B,long long C){
     long long ans=1;
     while (B){
@@ -24,6 +21,7 @@ long long power(long long A,long long B,long long C){
     }
     return ans;
 }
+
 //获取一组n个数论根
 void GetWn(){
     for(int i = 0; i < NUM; i++){
@@ -86,58 +84,82 @@ void Conv(long long a[], long long b[], long long c[],int n)
         c[i] = a[i] * b[i] % P;
     NTT(c, n, -1);
 }
-bool cmp(long long x,long long y){
-    return x>y;
+int top=0;
+long long st[40];
+//获取元根的两个函数
+void init(long long m){
+    top=0;
+    memset(st,0,sizeof st);
+    for (long long i=2;i<=sqrt(m)+1;i++){
+        if (m%i==0){
+            st[top++]=i;
+            while (m%i==0) m/=i;
+        }
+    }
+    if (m>1) st[top++]=m;
 }
-int main(){
-    fac[0]=fac[1]=inv[0]=facinv[0]=inv[1]=facinv[1]=POW[0]=1;
-    POW[1]=2;powinv[0]=1;powinv[1]=(P-P/2)%P;
-
-	for(int i = 2; i <= 100100; ++i) {
-        fac[i] = fac[i - 1] * i % P;
-        inv[i] = inv[P % i] * (P - P / i) % P;
-        facinv[i] = facinv[i - 1] * inv[i] % P;
-        POW[i]=POW[i-1]*2%P;
-        powinv[i]=powinv[i-1]*inv[2]%P;
+long long get_root(long long p){
+    init(p-1);
+    for (long long g=1;g<=p-1;g++){
+        bool bb=true;
+        for (int j=0;j<top;j++){
+            long long mod=power(g,(p-1)/st[j],p);
+            if (mod==1) {
+                bb=false;
+                break;
+            }
+        }
+        if (bb){
+            return g;
+        }
     }
-    GetWn();
-int t;
-cin>>t;
-while (t--){
-    int n;
-    scanf("%d",&n);
-    memset(A,0,sizeof A);
-    for (int i=1;i<=n;i++)
-        scanf("%lld",A+i);
-
-    sort(A+1,A+1+n,cmp);
-
-    for (int i=0;i<=n;i++){
-        a[i]=POW[n-i]*facinv[i]%P;
-        b[i]=A[n-i]*fac[n-i-1]%P;
-    }
-
+}
+long long pp[8010],r[32][8010*4];
+long long ans[8010*4],tmp[8010*4];
+int pn[8010][32];
+//为了方便数论变换，把运算集成到数组当中
+int solve(int m,long long aa[],long long bb[],long long c[]){
     int len=1;
-    while (len<=2*(n+1)) len<<=1;
-
-    for (int i=n+1;i<=len;i++)
+    while (len<=2*(m+1)) len<<=1;
+    long long a[len+10],b[len+10];
+    for (int i=0;i<m-1;i++){
+        a[i]=aa[i];
+        b[i]=bb[i];
+    }
+    for (int i=m-1;i<=len;i++)
         a[i]=b[i]=0;
-
-    memset(c,0,sizeof c);
     Conv(a,b,c,len);
 
-    for (int i=1;i<=n;i++)
-        f[i]=c[n-i];
-
-    for (int i=1;i<=n;i++)
-        f[i]=f[i]%P*facinv[i-1]%P*powinv[i]%P;
-
-    long long ans=0;
-    for (int i=1;i<=n;i++){
-        ans=(ans+f[i])%P;
-        printf("%lld\n",ans);
+    for (int i=0;i<m-1;i++){
+        c[i]=(c[i]+c[i+m-1])%P;
+        c[i+m-1]=0;
     }
-    printf("\n");
+    return len;
 }
+int main(){
+    long long n,m,x,s,X;
+
+    scanf("%lld%lld%lld%lld",&n,&m,&x,&s);
+    long long gg=get_root(m);
+    for (int i=1,j=0;j<m;i=i*gg%m,j++){
+        pp[i]=j%(m-1);
+    }
+
+    X=pp[x]%(m-1);
+    for (int i=0;i<s;i++){
+        int xx;
+        scanf("%d",&xx);
+        if (!xx) continue;
+        r[0][pp[xx]%(m-1)]=1;
+    }
+
+    GetWn();
+
+    for(ans[0]=1;n;n>>=1,solve(m,r[0],r[0],r[0]))
+        if(n&1){
+          solve(m,ans,r[0],ans);
+        }
+
+    printf("%lld\n",ans[X]);
     return 0;
 }
